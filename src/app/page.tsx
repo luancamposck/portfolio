7b384986405e24a972004561c0288f41c74152d4
luancamposck <luancamposck@gmail.com>
2026-03-23 12:21:59 -0700

    feat: US-021 - Compose Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import { AboutPreview } from "@/shared/components/sections/about-preview"
+import { CTA } from "@/shared/components/sections/cta"
+import { FeaturedProjects } from "@/shared/components/sections/featured-projects"
+import { Hero } from "@/shared/components/sections/hero"
+import { TechStack } from "@/shared/components/sections/tech-stack"
+
 const HomePage = () => {
-	return <h1>Hello World</h1>
+	return (
+		<>
+			<Hero />
+			<TechStack />
+			<FeaturedProjects />
+			<AboutPreview />
+			<CTA />
+		</>
+	)
 }
 
 export default HomePage
